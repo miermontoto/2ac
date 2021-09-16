4c8e0691c080c82fd8fa2103cfe09c0d6bafc19b
UO283319 <UO283319@uniovi.es>
2021-09-16 18:38:35 +0200

    1-1output

---
+++ sesion1-1/1-1output.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	int i = 3;
+	float f = 2.4;
+	char * s = "Content";
+	printf("The content of \"i\" is %d.\n", i);
+	printf("The content of \"f\" is %f.\n", f);
+	printf("The content of \"s\" is %s.\n", s);
+	return 0;
+}
