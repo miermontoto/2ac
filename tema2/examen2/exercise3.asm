b755a736c3dd639486d513ed2144b98a389e9c91
GijonDev <underscoreBis@gmail.com>
2021-12-05 14:38:33 +0100

    examen2 updated

---
+++ tema2/examen2/exercise3.asm
@@ -0,0 +1,19 @@
+.data
+	var: dword 12
+	res: dword 0
+
+.code
+main:
+	daddi 	r10, r0, 5		; r10 <- 5
+	daddi	r3, r0, 4		; r3 <- 4
+	daddi	r4, r6, 7 		; r4 <- 7
+	xor		r3, r3, r3
+	ld    	r5, var(r0) 	; load var in r5
+loop:
+	xor		r6, r6, r6		; r6 <- 0
+	ori		r2, r2, 2		; r2 <- 0
+	daddi  	r5, r5, -3 		; r5 <- r5 - 3 
+	beqz	r5, end			; checks if r5 is equals 0, and if so jumps to end label
+	j		loop
+end:
+	sd 		r5, res(r0)		; store r5
---
+++ tema2/examen2/exercise4.asm
@@ -0,0 +1,13 @@
+.code
+
+main:
+	ori 	r4, r0, 1	
+	daddi	r3, r2, 4	
+	xor		r1, r3, r5
+	ld		r9, 12(r5)
+	dmul 	r8, r5, r4
+	and		r5, r8, r4		
+	xor		r6, r2, r4
+	xor		r3, r3, r3
+	daddi	r3, r2, 3	
+	dmul	r8, r9, r4
